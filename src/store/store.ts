13a45738142213ee1a2026ce9fe88a34193001c4
Pachara Chantawong <pachara2202@gmail.com>
2021-08-16 22:23:26 +0700

    Implement store with socket

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,27 +1,44 @@
-import { InjectionKey } from 'vue'
-import { createStore, Store } from 'vuex'
+import { InjectionKey } from "vue";
+import { createStore, Store } from "vuex";
+import leaderboardObj from "../interface/leaderboardObj";
 
 // define your typings for the store state
 export interface State {
-  score: number,
-  click: boolean
+  oldscore: number;
+  score: number;
+  click: boolean;
+  leaderBoard: Array<leaderboardObj>;
 }
 
 // define injection key
-export const key: InjectionKey<Store<State>> = Symbol()
+export const key: InjectionKey<Store<State>> = Symbol();
 
 export const store = createStore<State>({
   state: {
-    score: Number(localStorage.getItem('score')),
-    click: false
+    oldscore: Number(localStorage.getItem("old_score")),
+    score: Number(localStorage.getItem("score")),
+    click: false,
+    leaderBoard: [],
   },
   mutations: {
-    increment (state) {
+    increment(state) {
       state.score++;
-      localStorage.setItem('score', String(state.score));
+      localStorage.setItem("score", String(state.score));
     },
-    click (state, val) {
-        state.click = val;
-    }
-  }
-})
+    click(state, val) {
+      state.click = val;
+    },
+    update_socket(state, socket) {
+      socket.emit("pop_data", {
+        country: "th",
+        score: state.score - state.oldscore,
+      });
+
+      state.oldscore = state.score;
+      localStorage.setItem("oldscore", String(state.oldscore));
+    },
+    setleaderBoard(state, data) {
+      state.leaderBoard = data;
+    },
+  },
+});
