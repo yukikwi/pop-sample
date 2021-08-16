55e55fb3be2aab848e9e8c543dbff733c58374c9
Pachara Chantawong <pachara2202@gmail.com>
2021-08-16 19:51:18 +0700

    Use socket.io-client and fix cors

--- socket_server/app.js
+++ socket_server/app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
 
 io.on('connection', (socket) => {
   console.log('a user connected');
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import App from "./App.vue";
 import { store, key } from './store/store'
 import './assets/main.css'
 import VueSocketIO from 'vue-3-socket.io'
+import SocketIO from 'socket.io-client'
 
 const app = createApp(App)
 app.use(store, key)
 app.use(new VueSocketIO({
     debug: true,
-    connection: 'http://localhost:3000',
+    connection: SocketIO('http://localhost:3000'),
     vuex: {
         store,
         actionPrefix: 'SOCKET_',
