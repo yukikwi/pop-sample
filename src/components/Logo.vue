e4fbf043ee4d2232f299012c818b478bc3889a7d
Pachara Chantawong <pachara2202@gmail.com>
2021-08-16 22:23:46 +0700

    Fix Lint

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -8,55 +8,56 @@
 </template>
 
 <script lang="ts">
-import { Vue, Watch } from 'vue-property-decorator'
-import { useStore } from 'vuex'
-import { key } from '../store/store'
+import { Vue, Watch } from "vue-property-decorator";
+import { useStore } from "vuex";
+import { key } from "../store/store";
 
 export default class Counter extends Vue {
-  store = useStore(key)
-  transition = 'pop1'
+  store = useStore(key);
+  transition = "pop1";
 
-  get score () {
-    return this.store.state.score
+  get score() {
+    return this.store.state.score;
   }
 
-  @Watch('score')
-  watchScore(){
-    if (this.transition === 'pop1'){
-      this.transition = 'pop2'
-    }
-    else if (this.transition === 'pop2'){
-      this.transition = 'pop1'
+  @Watch("score")
+  watchScore() {
+    if (this.transition === "pop1") {
+      this.transition = "pop2";
+    } else if (this.transition === "pop2") {
+      this.transition = "pop1";
     }
   }
 }
 </script>
 
 <style scoped>
-.pop1-enter-active, .pop1-leave-active {
-  animation: pop1 .3s;
+.pop1-enter-active,
+.pop1-leave-active {
+  animation: pop1 0.3s;
 }
 
 .pop1-leave-active {
   display: none;
 }
 
-.pop2-enter-active, .pop2-leave-active {
-  animation: pop2 .3s;
+.pop2-enter-active,
+.pop2-leave-active {
+  animation: pop2 0.3s;
 }
 
 .pop2-leave-active {
   display: none;
 }
 
-@keyframes pop1{
-  50%  {
+@keyframes pop1 {
+  50% {
     transform: scale(2) rotate(15deg);
   }
 }
 
-@keyframes pop2{
-  50%  {
+@keyframes pop2 {
+  50% {
     transform: scale(2) rotate(-15deg);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { store, key } from './store/store'
-import './assets/main.css'
-import VueSocketIO from 'vue-3-socket.io'
-import SocketIO from 'socket.io-client'
+import { store, key } from "./store/store";
+import "./assets/main.css";
+import VueSocketIO from "vue-3-socket.io";
+import SocketIO from "socket.io-client";
 
-const app = createApp(App)
-app.use(store, key)
-app.use(new VueSocketIO({
+const app = createApp(App);
+app.use(store, key);
+app.use(
+  new VueSocketIO({
     debug: true,
-    connection: SocketIO('http://localhost:3000'),
+    connection: SocketIO("http://localhost:3000"),
     vuex: {
-        store,
-        actionPrefix: 'SOCKET_',
-        mutationPrefix: 'SOCKET_'
-    }
-}))
+      store,
+      actionPrefix: "SOCKET_",
+      mutationPrefix: "SOCKET_",
+    },
+  })
+);
 app.mount("#app");
