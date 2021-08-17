666c154edc29ba9d1c8bd902ae00cea97dcc99ca
Pachara Chantawong <pachara2202@gmail.com>
2021-08-17 16:21:51 +0700

    Add cors origin for vercel

--- socket_server/app.js
+++ socket_server/app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
     cors: {
-        origin: "http://localhost:8080",
+        origin: ["http://localhost:8080", "https://pop-sample.vercel.app"],
         methods: ["GET", "POST"],
         credentials: true
     }
